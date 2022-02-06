defdbccb909656018276eeebec4a56ca9315f152
Daniil Shreyder <daniil.schroder@gmail.com>
2022-02-06 22:03:38 +0300

    Add random string generation util

---
+++ src/main/java/ru/red/four/authorizationservice/util/RandomStringUtil.java
@@ -0,0 +1,17 @@
+package ru.red.four.authorizationservice.util;
+
+import java.util.Random;
+
+public class RandomStringUtil {
+    public static String generateRandomString(int length) {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)) // Select only numbers and characters
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+}
